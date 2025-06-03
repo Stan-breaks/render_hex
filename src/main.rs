7a4a4360783bf3b18d7c578b2ae2e74899197b5c
Stan-breaks <stanleymwendwa03@gmail.com>
2025-06-04 01:12:31 +0300

    main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use std::env;
+
 use svg::Document;
 use svg::node::element::path::{Command, Data, Position};
-use svg::node::element::tag::Path;
 use svg::node::element::{Path, Rectangle};
 
 use crate::Operation::{Forward, Home, Noop, TurnLeft, TurnRight};
@@ -158,5 +159,13 @@ fn generate_svg(path_data: Vec<Command>) -> Document {
     document
 }
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let input = args.get(1).unwrap();
+    let default_filename = format!("{}.svg", input);
+    let save_to = args.get(2).unwrap_or(&default_filename);
+
+    let operations = parse(input);
+    let path_data = convert(&operations);
+    let document = generate_svg(path_data);
+    svg::save(save_to, &document).unwrap();
 }
